addfc7292948e6d68a554f53ea8905bd9fa4407c
gnemek20 <gnemek20@gmail.com>
2024-04-21 06:20:16 +0900

    section 속성 변경, section 전용 css 추가

--- components/section.tsx
+++ components/section.tsx
@@ -1,46 +1,15 @@
+import style from '@/styles/components/section/section.module.css'
 import { ReactNode, useEffect, useState } from 'react'
 
 interface sectionProps {
   children?: ReactNode
   className?: string
-  padding?: number
   gray?: boolean
 }
 
 const section = (props: sectionProps) => {
-  const [colorAttribute, setColorAttribute] = useState<string>('none');
-
-  useEffect(() => {
-    // let timeoutId: NodeJS.Timeout;
-    // const handleResize = () => {
-    //   clearTimeout(timeoutId);
-
-    //   timeoutId = setTimeout(() => {
-    //     let padding = props.padding || 0;
-    
-    //     if (window.innerWidth <= 767) padding /= 2;
-    //     setPaddingAttribute(`${padding}px 20px`);
-    //   }, 100);
-    // }
-
-    // handleResize();
-    // window.addEventListener('resize', handleResize);
-
-    const color = 'rgba(0, 0, 0, 0.05)';
-
-    if (props.gray) {
-      setColorAttribute(color);
-    }
-  }, [])
-
   return (
-    <div
-      className={`${props.className}`}
-      style={{
-        padding: props.padding,
-        backgroundColor: colorAttribute,
-      }}
-    >
+    <div className={`${props.className} ${props.gray && style.gray} ${style.section}`}>
       { props.children }
     </div>
   )
