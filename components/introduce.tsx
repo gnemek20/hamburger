0df22c0f91f993d4d8d13b9a37cbaadaf6c5b383
gnemek20 <gnemek20@gmail.com>
2024-04-21 07:44:28 +0900

    소개 섹션에 애니메이션 추가

--- components/introduce.tsx
+++ components/introduce.tsx
@@ -1,9 +1,14 @@
 import style from '@/styles/components/introduce/introduce.module.css'
 import animation from '@/styles/components/introduce/animation.module.css'
 import { ImageFrame, Section } from '.'
-import Image from 'next/image'
+import { RefObject, useEffect } from 'react'
 
-const introduce = () => {
+interface introduceProps {
+  elementRef: RefObject<HTMLDivElement>
+  startAnimation: boolean
+}
+
+const introduce = (props: introduceProps) => {
   const thumbnailImage = {
     src: require('@/public/images/shoe.jpg'),
     alt: 'thumbnailImage'
@@ -11,15 +16,15 @@ const introduce = () => {
   
   return (
     <Section className={`flex justifyCenter`} gray>
-      <div className={`flex limitWidth maxWidth alignEnd mobileAlignStart mobileFlexColumn`}>
+      <div ref={props.elementRef} className={`flex limitWidth maxWidth alignEnd mobileAlignStart mobileFlexColumn`}>
         <div className={`flex flexColumn ${style.content}`}>
-          <h1 className={`title`}>샘플 텍스트</h1>
-          <div>
+          <h1 className={`title opacityNone ${props.startAnimation && animation.titleSlideIn}`}>샘플 텍스트</h1>
+          <div className={`opacityNone ${props.startAnimation && animation.textSlideIn}`}>
             <p className={`text`}>회사에 대한 설명</p>
             <p className={`text`}>의도적인 여러줄도 가능함</p>
           </div>
         </div>
-        <ImageFrame className={`${style.thumbnail}`} image={thumbnailImage} />
+        <ImageFrame className={`opacityNone ${style.thumbnail} ${props.startAnimation && animation.thumbnailSlideIn}`} image={thumbnailImage} />
       </div>
     </Section>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
 import style from "@/styles/main.module.css"
-import { Introduce, Landing, Section } from '@/components/index';
+import { Introduce, Landing } from '@/components/index';
+import { RefObject, useEffect, useRef, useState } from "react";
 
 const main = () => {
+  const [intersectingElement, setIntersectingElement] = useState<Array<RefObject<HTMLDivElement>>>([]);
+  const introduceRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (introduceRef) {
+      const observer = new IntersectionObserver(([event]) => {
+        if (event.isIntersecting) {
+          if (!intersectingElement.includes(introduceRef)) setIntersectingElement([...intersectingElement, introduceRef]);
+          else introduceRef.current && observer.unobserve(introduceRef.current);
+        }
+      }, { threshold: 0.5 })
+
+      if (introduceRef.current) observer.observe(introduceRef.current);
+    }
+  }, [introduceRef])
+
   return (
     <>
-      <Landing></Landing>
-      <Introduce></Introduce>
+      <Landing />
+      <Introduce elementRef={introduceRef} startAnimation={intersectingElement.includes(introduceRef)} />
     </>
   );
 }
