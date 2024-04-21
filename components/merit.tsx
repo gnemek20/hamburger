5bd7ae2f613c901f1a35d9746e410c97c50cd225
gnemek20 <gnemek20@gmail.com>
2024-04-21 13:25:48 +0900

    장점 섹션 애니메이션 추가

--- components/merit.tsx
+++ components/merit.tsx
@@ -12,17 +12,17 @@ const merit = (props: meritProps) => {
   return (
     <Section className={`flex justifyCenter`} gray>
       <div className={`limitWidth maxWidth ${style.container}`} ref={props.elementRef}>
-        <div className={`${style.panel}`}>
+        <div className={`opacityNone ${style.panel} ${animation.default} ${props.startAnimation && animation.firstPanelSlideIn}`}>
           <div className={`${style.panelPit}`} />
           <h1 className={`mobileTitle`}>샘플 강조 텍스트</h1>
           <h1 className={`text ${style.panelTag}`}>#Sample</h1>
         </div>
-        <div className={`${style.panel}`}>
+        <div className={`opacityNone ${style.panel} ${animation.default} ${props.startAnimation && animation.secondPanelSlideIn}`}>
           <div className={`${style.panelPit}`} />
           <h1 className={`mobileTitle`}>회사에서 내세울 강점을 한 줄 내외로 서술해주세요</h1>
           <h1 className={`text ${style.panelTag}`}>#Company</h1>
         </div>
-        <div className={`${style.panel}`}>
+        <div className={`opacityNone ${style.panel} ${animation.default} ${props.startAnimation && animation.thirdPanelSlideIn}`}>
           <div className={`${style.panelPit}`} />
           <h1 className={`mobileTitle`}>텍스트 크기 조절도 가능합니다</h1>
           <h1 className={`text ${style.panelTag}`}>#Resize</h1>
