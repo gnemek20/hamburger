734a5753eb4991780be719e6fa3cc83c15ae8eb3
gnemek20 <gnemek20@gmail.com>
2024-09-06 22:45:03 +0900

    지퍼 구조 예시 섹션 추가

--- components/zipperStructure.tsx
+++ components/zipperStructure.tsx
@@ -1,8 +1,9 @@
 import style from '@/styles/components/zipperStructure/zipperStructure.module.css'
-
-const backgroundImage = require('@/public/images/blueprint.jpg');
+import Flicking from '@egjs/react-flicking';
 
 const zipperStructure = () => {
+  const array = new Array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+
   return (
     <div className={`flex justifyCenter ${style.zipperStructureBackground}`}>
       <div className={`limitWidth maxWidth ${style.zipperStructure}`}>
@@ -17,8 +18,28 @@ const zipperStructure = () => {
             <p className={`mobileText`}>풀러</p>
           </div>
         </div>
-        <div>
-
+        <div className={`${style.flickingBackground}`}>
+          <div className={`${style.shadow}`}>
+            <div />
+            <div />
+          </div>
+          <Flicking align={'center'} circular>
+            {
+              array[0].map((item, index) => (
+                <div key={index} className={`${style.imageFrame}`}>
+                  <div className={`${style.image}`}>
+                    <p>{ index }</p>
+                  </div>
+                </div>
+              ))
+            }
+          </Flicking>
+        </div>
+        <div className={`${style.description}`}>
+          <p className={`text`}>이름</p>
+          <div>
+            <p className={`mobileText`}>이거에 대한 설명들</p>
+          </div>
         </div>
       </div>
     </div>
