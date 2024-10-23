0d23e6e503121bee6061cb0aeb608eeee046f2f5
gnemek20 <gnemek20@gmail.com>
2024-10-23 21:26:49 +0900

    메리트 섹션 디자인 추가

--- components/merit.tsx
+++ components/merit.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import style from '@/styles/components/merit/merit.module.css'
 import { Banner, Offset } from ".";
 
 const bannerImage = {
@@ -6,11 +7,43 @@ const bannerImage = {
   alt: 'bannerImage'
 }
 
+const runnerIcon = {
+  src: require('@/public/icons/runner.svg'),
+  alt: 'runner'
+}
+
 const merit = () => {
+  interface meritAttribute {
+    title: string
+    subTitle: string,
+    icon: typeof bannerImage
+  }
+
+  const meritList = new Array(
+    {
+      title: '속도',
+      subTitle: '속도속도',
+      icon: runnerIcon
+    }
+  );
+
   return (
     <>
       <Offset size={75} />
-      <Banner image={bannerImage} title="회사의 가치" subTitle="Merit of Company" />
+      <Banner image={bannerImage} title="대양ING의 강점" subTitle="Merit of DaeYangING" />
+      <div className={`${style.meritList}`}>
+        {
+          meritList.map((merit, index) => (
+            <div className={`${style.merit}`} key={index}>
+              <div className={`${style.meritIcon}`}>
+                <Image src={merit.icon.src} alt={merit.icon.alt} /> 
+              </div>
+              <p className="text">{ merit.title }</p>
+              <p className="mobileText">{ merit.subTitle }</p>
+            </div>
+          ))
+        }
+      </div>
     </>
   );
 }
