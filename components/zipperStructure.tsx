f168dde48920e5a047436586daebc66c53383133
gnemek20 <gnemek20@gmail.com>
2024-10-27 11:49:08 +0900

    내용 주석 처리

--- components/zipperStructure.tsx
+++ components/zipperStructure.tsx
@@ -46,10 +46,10 @@ const Stack = ({cardArray, observed}: stackAttribute) => {
       {
         cardArray.map((card, index) => (
           <div className={`${style.card} ${compareToSelectedCard(card.name) && style.selected}`} onClick={() => changeSelectedCard(card.name)} key={index}>
-            <div className={`${style.cardImage}`}>
+            {/* <div className={`${style.cardImage}`}>
               <Image src={card.image.src} alt={card.image.alt} />
             </div>
-            <p className={`${compareToSelectedCard(card.name) ? 'text' : 'mobileText'}`}>{ card.name }</p>
+            <p className={`${compareToSelectedCard(card.name) ? 'text' : 'mobileText'}`}>{ card.name }</p> */}
           </div>
         ))
       }
